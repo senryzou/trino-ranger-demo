0542bbfcca4d90ab642310d55970b49eb32cc05a
senryzou <senry@163.com>
2024-05-31 02:50:49 -0700

    Update docker-entrypoint.sh

--- ranger-admin/docker-entrypoint.sh
+++ ranger-admin/docker-entrypoint.sh
@@ -20,5 +20,5 @@ cp /root/ranger-admin/install.properties /root/ranger-${RANGER_VERSION}-admin/ &
 ./setup.sh && \
 ranger-admin start && \
 python -m venv /root/ranger-admin/.env && source /root/ranger-admin/.env/bin/activate && pip install -r /root/ranger-admin/requirement.txt
-python /root/ranger-admin/trino_service_setup.py && \
+# python /root/ranger-admin/trino_service_setup.py && \
 tail -f /root/ranger-${RANGER_VERSION}-admin/ews/logs/ranger-admin-*-.log
