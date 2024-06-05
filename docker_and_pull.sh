32756dfb4055b4b8877140139ddace64f640081f
senryzou <senry@163.com>
2024-06-04 23:32:46 -0700

    Update docker_and_pull.sh

--- docker_and_pull.sh
+++ docker_and_pull.sh
@@ -1,5 +1,5 @@
-snap install docker -y
-
+#snap install docker -y
+bash docker_engine_ubuntu.sh
 cat docker-compose.yml |grep -v '#'|grep image|awk '{print "docker pull ",$2}' >pull_image.sh
 
 bash pull_image.sh
