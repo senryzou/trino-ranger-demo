9b67bb3c322315ad3eb582e9064f7a5cb667552e
senryzou <senry@163.com>
2024-06-05 00:07:50 -0700

    Update trino_service_setup.py

--- ranger-admin/trino_service_setup.py
+++ ranger-admin/trino_service_setup.py
@@ -8,7 +8,10 @@ ranger_url  = 'https://localhost:6182'
 ranger_auth = ('admin', 'Rangeradmin1')
 
 ranger = RangerClient(ranger_url, ranger_auth)
+
+# to disable SSL certificate validation (not recommended for production use!)
 ranger.session.verify = False
+
 trino_service_file=open('/root/ranger-admin/trino-service.json')
 trino_service=json.load(trino_service_file)
 
