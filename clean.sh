3bbfba4fb256bc87a16da1b1e092b2e9484c5d4a
senryzou <senry@163.com>
2024-06-05 19:12:54 -0700

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,10 +1,13 @@
 set +e
 docker compose down
+
 docker ps -a |grep ago |awk '{print $1}'|xargs docker rm -f
 #docker images |grep ago |awk '{print $3}'|xargs docker rmi -f
 docker system prune --volumes -f
+rm -rf /var/lib/containerd
 systemctl stop docker
 systemctl stop docker.socket
 systemctl start docker
 docker system prune --volumes -f
+
 set -e
