5f1eee5528e10c983b05610125e909a27d366904
senryzou <senry@163.com>
2024-06-05 19:19:53 -0700

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -5,8 +5,9 @@ docker ps -a |grep ago |awk '{print $1}'|xargs docker rm -f
 #docker images |grep ago |awk '{print $3}'|xargs docker rmi -f
 docker system prune --volumes -f
 rm -rf /var/lib/containerd
+rm -rf /var/lib/docker/volumes/trino*.*
 systemctl stop docker
-systemctl stop docker.socket
+systmctl stop docker.socket
 systemctl start docker
 docker system prune --volumes -f
 
